8f93867b1e09105b712d01db648a77da3b05bb6e
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2019-10-08 18:43:49 +0100

    work in progress

---
+++ T1/MyComponent.js
@@ -0,0 +1,16 @@
+/**
+ * MyComponent
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+
+class MyComponent extends CGFobject {
+
+    constructor(scene, id, transformations = mat4.create(), primitives = [], components = []){
+        super(scene);
+        this.id = id;
+        this.transformations = transformations;
+        this.primitives = primitives;
+        this.components = components;
+    }
+}
